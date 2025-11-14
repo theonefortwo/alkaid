3c7133191b033d0205dfc06bb609354b64aff067
the142 <pendulu.casual@gmail.com>
2025-11-14 00:33:00 -0800

    Add --target-name argument

--- alkaid/main.py
+++ alkaid/main.py
@@ -6,7 +6,7 @@ from .symbols import Symbols
 from .fingerprint import generate_print, instruction_count
 from .demangle import demangle, DemangleStateException
 
-def alkaid_proc(path_to_dol, path_to_symbols):
+def alkaid_proc(path_to_dol, path_to_symbols, target_name=None):
 	f = open(path_to_dol, 'rb')
 	dolfile = DOLFile(f)
 	f.close()
@@ -27,6 +27,8 @@ def alkaid_proc(path_to_dol, path_to_symbols):
 		function_prints.append(function_print)
 	for i in range(len(function_names)):
 		function_name = function_names[i]
+		if target_name is not None and target_name not in function_name:
+			continue
 		function_print = function_prints[i]
 		for j in range(len(function_names)):
 			if i == j:
@@ -34,7 +36,7 @@ def alkaid_proc(path_to_dol, path_to_symbols):
 			new_print = function_prints[j]
 			new_name = function_names[j]
 			npic = instruction_count(new_print)
-			if npic>15 and new_print in function_print and new_print != function_print:
+			if npic>16 and new_print[4:-4] in function_print and new_print != function_print:
 				print(f"[confidence:{npic}] {new_name} is likely inlined in {function_name}")
 
 def main():
@@ -52,5 +54,11 @@ def main():
 		type=str,
 		help='Path to the \'symbols.txt\' file.'
 	)
+	parser.add_argument(
+		'--target-name',
+		type=str,
+		default=None,
+		help='The target function name to search for.'
+	)
 	args = parser.parse_args()
-	alkaid_proc(args.path_to_dol, args.path_to_symbols)
+	alkaid_proc(args.path_to_dol, args.path_to_symbols, target_name=args.target_name)
